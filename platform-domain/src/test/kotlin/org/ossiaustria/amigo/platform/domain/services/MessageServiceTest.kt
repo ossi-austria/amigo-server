3373587679220707b43b01365a1d30d85af33980
Florian Hintermeier <florian@hintermeier.at>
2021-05-13 00:33:56 +0200

    closes amigo-project#154: Add Tests for MessageService

--- platform-domain/src/test/kotlin/org/ossiaustria/amigo/platform/domain/services/MessageServiceTest.kt
+++ platform-domain/src/test/kotlin/org/ossiaustria/amigo/platform/domain/services/MessageServiceTest.kt
@@ -7,42 +7,28 @@ import org.junit.jupiter.api.assertThrows
 import org.ossiaustria.amigo.platform.domain.models.Message
 import org.ossiaustria.amigo.platform.domain.models.ValidationException
 import org.ossiaustria.amigo.platform.domain.repositories.MessageRepository
-import org.ossiaustria.amigo.platform.domain.testcommons.Models
-import org.ossiaustria.amigo.platform.exceptions.NotFoundException
+import org.ossiaustria.amigo.platform.domain.services.sendables.SendableServiceTest
 import org.springframework.beans.factory.annotation.Autowired
 import java.util.*
-import java.util.UUID.randomUUID
 
-internal class MessageServiceTest : AbstractServiceTest() {
+internal class MessageServiceTest : SendableServiceTest<Message, MessageService>() {
 
     @Autowired
-    private lateinit var service: MessageService
+    override lateinit var service: MessageService
 
     @Autowired
     private lateinit var messageRepository: MessageRepository
 
 
-    val personId1: UUID = randomUUID()
-    val personId2: UUID = randomUUID()
-    val personId3: UUID = randomUUID()
-    val groupId1: UUID = randomUUID()
-    val groupId2: UUID = randomUUID()
-
     @BeforeEach
     fun beforeEach() {
         cleanTables()
         messageRepository.deleteAll()
-        groups.save(Models.group(groupId1))
-        groups.save(Models.group(groupId2))
-        accounts.save(Models.account()).also {
-            persons.save(Models.persons(personId1, it.id, groupId1))
-        }
-        accounts.save(Models.account()).also {
-            persons.save(Models.persons(personId2, it.id, groupId1))
-        }
-        accounts.save(Models.account()).also {
-            persons.save(Models.persons(personId3, it.id, groupId2))
-        }
+
+        mockPersons()
+
+        messageRepository.save(Message(existingId, personId1, personId2, "text"))
+        messageRepository.save(Message(UUID.randomUUID(), personId2, personId1, "text"))
 
     }
 
@@ -73,121 +59,5 @@ internal class MessageServiceTest : AbstractServiceTest() {
         }
     }
 
-    @Test
-    fun `getOne should throw when id does not exist`() {
-        assertThrows<NotFoundException> {
-            service.getOne(randomUUID())
-        }
-    }
-
-    @Test
-    fun `getOne should succeed for existing id`() {
-        val id = randomUUID()
-        messageRepository.save(Message(id, personId1, personId2, "text"))
-        val result = service.getOne(id)
-        assertThat(result).isNotNull
-        assertThat(result.id).isEqualTo(id)
-    }
-
-    @Test
-    fun `getAll should succeed generally`() {
-        val id = randomUUID()
-        messageRepository.save(Message(id, personId1, personId2, "text"))
-        val result = service.getAll()
-        assertThat(result).isNotNull
-        assertThat(result.size).isEqualTo(1)
-        assertThat(result.first().id).isEqualTo(id)
-
-    }
-
-    @Test
-    fun `findWithPersons should succeed for persons in same group`() {
-        messageRepository.save(Message(randomUUID(), personId1, personId2, "text"))
-        messageRepository.save(Message(randomUUID(), personId2, personId1, "text"))
-
-        val result = service.findWithPersons(personId1, personId2)
-        assertThat(result).isNotNull
-        assertThat(result.size).isEqualTo(1)
-
-    }
-
-    @Test
-    fun `findWithPersons should throw when personsId are both null`() {
-        assertThrows<SendableError.PersonsNotProvided> {
-            service.findWithPersons(null, null)
-        }
-    }
-
-    @Test
-    fun `findWithPersons should succeed when senderId is set`() {
-        messageRepository.save(Message(randomUUID(), personId1, personId2, "text"))
-        messageRepository.save(Message(randomUUID(), personId2, personId1, "text"))
-
-        val result = service.findWithPersons(personId1, null)
-        assertThat(result).isNotNull
-        assertThat(result.size).isEqualTo(1)
-    }
-
-    @Test
-    fun `findWithPersons should succeed when receiverId is set`() {
-        messageRepository.save(Message(randomUUID(), personId1, personId2, "text"))
-        messageRepository.save(Message(randomUUID(), personId2, personId1, "text"))
-
-        val result = service.findWithPersons(null, personId1)
-        assertThat(result).isNotNull
-        assertThat(result.size).isEqualTo(1)
-    }
-
-    @Test
-    fun `findWithSender should succeed generally`() {
-        messageRepository.save(Message(randomUUID(), personId1, personId2, "text"))
-        messageRepository.save(Message(randomUUID(), personId2, personId1, "text"))
-
-        val result = service.findWithSender(personId1)
-        assertThat(result).isNotNull
-        assertThat(result.size).isEqualTo(1)
-    }
-
-    @Test
-    fun `findWithReceiver should succeed generally`() {
-        messageRepository.save(Message(randomUUID(), personId1, personId2, "text"))
-        messageRepository.save(Message(randomUUID(), personId2, personId1, "text"))
-
-        val result = service.findWithReceiver(personId1)
-        assertThat(result).isNotNull
-        assertThat(result.size).isEqualTo(1)
-    }
-
-    @Test
-    fun `markAsSent should throw when id does not exist`() {
-        assertThrows<NotFoundException> {
-            service.markAsSent(randomUUID())
-        }
-    }
-
-    @Test
-    fun `markAsSent should succeed for existing id`() {
-        val id = randomUUID()
-        messageRepository.save(Message(id, personId1, personId2, "text"))
-        val result = service.markAsSent(id)
-        assertThat(result).isNotNull
-        assertThat(result.sendAt).isNotNull
-    }
-
-    @Test
-    fun `markAsRetrieved should throw when id does not exist`() {
-        assertThrows<NotFoundException> {
-            service.markAsRetrieved(randomUUID())
-        }
-    }
-
-    @Test
-    fun `markAsRetrieved should succeed for existing id`() {
-        val id = randomUUID()
-        messageRepository.save(Message(id, personId1, personId2, "text"))
-        val result = service.markAsRetrieved(id)
-        assertThat(result).isNotNull
-        assertThat(result.retrievedAt).isNotNull
-    }
 
 }
---
+++ platform-domain/src/test/kotlin/org/ossiaustria/amigo/platform/domain/services/sendables/SendableServiceTest.kt
@@ -0,0 +1,134 @@
+package org.ossiaustria.amigo.platform.domain.services.sendables
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.ossiaustria.amigo.platform.domain.models.Sendable
+import org.ossiaustria.amigo.platform.domain.services.AbstractServiceTest
+import org.ossiaustria.amigo.platform.domain.services.SendableError
+import org.ossiaustria.amigo.platform.domain.services.SendableService
+import org.ossiaustria.amigo.platform.domain.testcommons.Models
+import org.ossiaustria.amigo.platform.exceptions.NotFoundException
+import java.util.*
+import java.util.UUID.randomUUID
+
+internal abstract class SendableServiceTest<S : Sendable<S>, T : SendableService<S>> : AbstractServiceTest() {
+
+    protected abstract var service: T
+
+    val existingId: UUID = randomUUID()
+
+    val personId1: UUID = randomUUID()
+    val personId2: UUID = randomUUID()
+    val personId3: UUID = randomUUID()
+
+    val groupId1: UUID = randomUUID()
+    val groupId2: UUID = randomUUID()
+
+    protected fun mockPersons() {
+        groups.save(Models.group(groupId1))
+        groups.save(Models.group(groupId2))
+        accounts.save(Models.account()).also {
+            persons.save(Models.persons(personId1, it.id, groupId1))
+        }
+        accounts.save(Models.account()).also {
+            persons.save(Models.persons(personId2, it.id, groupId1))
+        }
+        accounts.save(Models.account()).also {
+            persons.save(Models.persons(personId3, it.id, groupId2))
+        }
+    }
+
+    @Test
+    fun `getOne should throw when id does not exist`() {
+        assertThrows<NotFoundException> {
+            service.getOne(randomUUID())
+        }
+    }
+
+    @Test
+    fun `getOne should succeed for existing id`() {
+        val result = service.getOne(existingId)
+        assertThat(result).isNotNull
+        assertThat(result.id).isEqualTo(existingId)
+    }
+
+    @Test
+    fun `getAll should succeed generally`() {
+        val result = service.getAll()
+        assertThat(result).isNotNull
+        assertThat(result.size).isEqualTo(2)
+    }
+
+    @Test
+    fun `findWithPersons should succeed for persons in same group`() {
+        val result = service.findWithPersons(personId1, personId2)
+        assertThat(result).isNotNull
+        assertThat(result.size).isEqualTo(1)
+
+    }
+
+    @Test
+    fun `findWithPersons should throw when personsId are both null`() {
+        assertThrows<SendableError.PersonsNotProvided> {
+            service.findWithPersons(null, null)
+        }
+    }
+
+    @Test
+    fun `findWithPersons should succeed when senderId is set`() {
+        val result = service.findWithPersons(personId1, null)
+        assertThat(result).isNotNull
+        assertThat(result.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `findWithPersons should succeed when receiverId is set`() {
+        val result = service.findWithPersons(null, personId1)
+        assertThat(result).isNotNull
+        assertThat(result.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `findWithSender should succeed generally`() {
+        val result = service.findWithSender(personId1)
+        assertThat(result).isNotNull
+        assertThat(result.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `findWithReceiver should succeed generally`() {
+        val result = service.findWithReceiver(personId1)
+        assertThat(result).isNotNull
+        assertThat(result.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `markAsSent should throw when id does not exist`() {
+        assertThrows<NotFoundException> {
+            service.markAsSent(randomUUID())
+        }
+    }
+
+    @Test
+    fun `markAsSent should succeed for existing id`() {
+        val result = service.markAsSent(existingId)
+        assertThat(result).isNotNull
+        assertThat(result.sendAt).isNotNull
+    }
+
+    @Test
+    fun `markAsRetrieved should throw when id does not exist`() {
+        assertThrows<NotFoundException> {
+            service.markAsRetrieved(randomUUID())
+        }
+    }
+
+    @Test
+    fun `markAsRetrieved should succeed for existing id`() {
+        val result = service.markAsRetrieved(existingId)
+        assertThat(result).isNotNull
+        assertThat(result.retrievedAt).isNotNull
+    }
+
+}
