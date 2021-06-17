1bb98f120221df8a8ea43a1c822d0881a121ac9d
Florian Hintermeier <florian@hintermeier.at>
2021-06-17 14:40:28 +0200

    Feature #154: Fixing startup classes

--- Dockerfile
+++ Dockerfile
@@ -10,19 +10,12 @@ WORKDIR /workdir
 # copy the sources to image (except .dockerignore)
 ADD . /workdir
 RUN gradle -x test :platform-domain:jar :platform-rest:bootJar -x :platform-rest:asciidoctor
-#RUN cp /workdir/platform-domain/build/dependency/db /workdir/platform-rest/build/dependency/BOOT-INF/classes/ -r
-#RUN cp /workdir/platform-domain/build/dependency/org /workdir/platform-rest/build/dependency/BOOT-INF/classes/ -r
 
 # Start a new docker stage here, and only copy the finished build artefacts.
 FROM openjdk:8-jdk-alpine
 RUN addgroup -S spring && adduser -S spring -G spring
 USER spring:spring
-# add the gradle dependencies and own artificats in a docker-friendly way
-#COPY --from=BUILDER /workdir/platform-rest/build/dependency/BOOT-INF/lib     /app/lib
-#COPY --from=BUILDER /workdir/platform-rest/build/dependency/META-INF         /app/META-INF
-#COPY --from=BUILDER /workdir/platform-rest/build/dependency/BOOT-INF/classes /app
 COPY --from=BUILDER /workdir/platform-rest/build/libs/*.jar /app.jar
 
 # start app
-#ENTRYPOINT ["java","-cp","app:app/lib/*","org.ossiaustria.amigo.platform.RestApplicationKt"]
 ENTRYPOINT ["java","-jar","/app.jar"]
--- platform-rest/src/main/kotlin/org/ossiaustria/amigo/platform/config/FirebaseMessagingConfig.kt
+++ platform-rest/src/main/kotlin/org/ossiaustria/amigo/platform/config/FirebaseMessagingConfig.kt
@@ -11,27 +11,35 @@ import org.ossiaustria.amigo.platform.services.messaging.FirebaseMessagingServic
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.FileUrlResource
+import java.util.logging.Logger
 
 @Configuration
 @Profile(ApplicationProfiles.NOT_TEST)
 class FirebaseMessagingConfig {
 
     @Bean
-    fun firebaseMessaging(): FirebaseMessaging {
-        val googleCredentials = GoogleCredentials
-            .fromStream(ClassPathResource("firebase-service-account.json").inputStream)
-        val firebaseOptions = FirebaseOptions
-            .builder()
-            .setCredentials(googleCredentials)
-            .build()
-        val app = FirebaseApp.initializeApp(firebaseOptions, "my-app")
-        return FirebaseMessaging.getInstance(app)
-    }
+    fun messagingService(): MessagingService =
+        try {
+            val googleCredentials = GoogleCredentials
+                .fromStream(FileUrlResource(FIREBASE_CONFIG_FILE).inputStream)
+            val firebaseOptions = FirebaseOptions
+                .builder()
+                .setCredentials(googleCredentials)
+                .build()
+            val app = FirebaseApp.initializeApp(firebaseOptions, "my-app")
+            val firebaseMessaging = FirebaseMessaging.getInstance(app)
+            FirebaseMessagingService(firebaseMessaging)
+        } catch (e: Exception) {
+            log.warning("# MISSING CONFIG: $FIREBASE_CONFIG_FILE")
+            log.warning("# Cannot use Firebase messaging for this server instance")
+            NoopMessagingService()
+        }
 
-    @Bean
-    fun firebaseMessagingService(firebaseMessaging: FirebaseMessaging): MessagingService {
-        return FirebaseMessagingService(firebaseMessaging)
+
+    companion object {
+        private val log: Logger = Logger.getLogger(this::class.simpleName)
+        private const val FIREBASE_CONFIG_FILE: String = "configs/firebase-service-account.json"
     }
 }
 
